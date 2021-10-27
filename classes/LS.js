44147086b04800dc837ac7d1b0f9700eca759fa2
MKougia <kougia.mihkel@khk.ee>
2021-10-27 13:01:10 +0300

    Refactored LS data getting and setting functionality

--- classes/LS.js
+++ classes/LS.js
@@ -1,14 +1,25 @@
 class LS {
-	addTask(task){
-		// set data to the LS
-		let tasks;
-		if(localStorage.getItem('tasks') === null) {
-			tasks = [];
+	// help functions to get and set data at LS
+	getData(name) {
+		let data;
+		if(localStorage.getItem(name) === null) {
+			data = [];
 		} else {
-			tasks = JSON.parse(localStorage.getItem('tasks'));
+			data = JSON.parse(localStorage.getItem(name));
 		}
+		return data;
+	}
+
+	setData(name, data) {
+		localStorage.setItem(name, JSON.stringify(data));
+	}
+
+	// add task to LS
+	addTask(task) {
+		// set data to the LS
+		let tasks = this.getData('tasks');
 		tasks.push(task);
-		localStorage.setItem('tasks', JSON.stringify(tasks));
+		this.setData('tasks', tasks);
 		// log to console that task is added to LS
 		task.addedToLS();
 	}
